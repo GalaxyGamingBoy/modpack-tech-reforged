f1c1db8b70082b96b8b7ff5a8ede1ef17d4a3cdf
Marios Mitsios <xrteach@gmail.com>
2023-10-29 13:50:14 +0200

    feat(server): Create Tunnel Notifier

--- server/createTunnel.py
+++ server/createTunnel.py
@@ -1,4 +1,5 @@
-import shutil, os
+import shutil, requests, subprocess, time, sys
+from datetime import datetime
 
 
 def cloudflared_exists():
@@ -10,10 +11,45 @@ def cloudflared_exists():
 
 def create_track_tunnel():
     print("Creating a create-track-mod website tunnel...")
-    os.system("cloudflared tunnel --url http://localhost:3876/ --no-autoupdate")
+    proc = subprocess.Popen(
+        "cloudflared tunnel --url http://localhost:3876/ --no-autoupdate --metrics localhost:5555",
+        shell=True,
+    )
     print("Done!")
+    return proc
+
+
+def get_hostname():
+    print("Getting Hostname...")
+    hostname = (requests.get("http://localhost:5555/quicktunnel").json())["hostname"]
+    print(f"Got hostname, {hostname}!")
+    return hostname
+
+
+def notify(token, chat_id, hostname):
+    print("Notifying via telegram...")
+    requests.post(
+        f"https://api.telegram.org/bot{token}/sendMessage",
+        data={
+            "chat_id": chat_id,
+            "text": f"""
+~~~ Server Tunnel Notifier ~~~
+DateTime: {datetime.now()}
+
+Create Tunnel: {hostname}
+""",
+        },
+    )
 
 
 if __name__ == "__main__":
     if cloudflared_exists():
-        create_track_tunnel()
+        PROC = create_track_tunnel()
+        time.sleep(5)
+        HOSTNAME = get_hostname()
+
+        if sys.argv[1] != "-1":
+            notify(sys.argv[1].split("|")[0], sys.argv[1].split("|")[1], HOSTNAME)
+
+        input("Press any key to exit...")
+        PROC.kill()
